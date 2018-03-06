dfa5ee4ba091f3c4fd5a65a10856680341186500
David Ramlakhan <dram1964@gmail.com>
2018-03-06 18:34:10 +0000

    Updated POD and test scripts for OpenEHR::REST::EHR. Added update_ehr_status method

--- lib/OpenEHR/REST/EHR.pm
+++ lib/OpenEHR/REST/EHR.pm
@@ -10,97 +10,140 @@ extends 'OpenEHR::REST';
 
 use version; our $VERSION = qv('0.0.2');
 
-has subjectId        => ( is => 'rw', isa => 'Str', required => 0 );
-has subjectNamespace => ( is => 'rw', isa => 'Str', required => 0 );
-has committerName    => ( is => 'rw', isa => 'Str', required => 0 );
+has subject_id        => ( is => 'rw', isa => 'Str' );
+has subject_namespace => ( is => 'rw', isa => 'Str' );
+has committer_name    => ( is => 'rw', isa => 'Str' );
+has committer_id      => ( is => 'rw', isa => 'Str' );
 
-has resource => ( is => 'rw', isa => 'Str', default => 'ehr' );
-has ehrId    => ( is => 'rw', isa => 'Str' );
-has action   => ( is => 'rw', isa => 'Str' );
-has ehrStatus => ( is => 'rw', isa => 'HashRef' );
-has err_msg   => ( is => 'rw', isa => 'Str' );
+has resource          => ( is => 'rw', isa => 'Str', default  => 'ehr' );
+
+has ehr_id  => ( is => 'ro', isa => 'Str', writer => '_set_ehr_id'  );
+has action  => ( is => 'ro', isa => 'Str', writer => '_set_action'  );
+has err_msg => ( is => 'ro', isa => 'Str', writer => '_set_err_msg' );
+has href    => ( is => 'ro', isa => 'Str', writer => '_set_href'    );
+has ehr_status => (
+    is     => 'ro',
+    isa    => 'HashRef',
+    writer => '_set_ehr_status',
+);
 
 sub find_or_new {
     my $self = shift;
-    croak unless $self->subjectId;
-    croak unless $self->subjectNamespace;
-    croak unless $self->committerName;
-    if ( $self->_find_by_subject_id() ) {
+    $self->_check_query;
+    if ( $self->find_by_subject_id() ) {
         return 1;
     }
-    elsif ( $self->_get_new_ehr() ) {
-        return 2;
+    elsif ( $self->get_new_ehr() ) {
+        return 1;
     }
     else {
-        croak $self->err_msg;
         return 0;
     }
 }
 
-sub find_by_ehrid {
-    my ( $self, $ehrid ) = @_;
-    $self->resource("ehr/$ehrid");
-    $self->submit_rest_call;
-    if ( $self->response_code eq '200' ) {
-        my $response = from_json( $self->response );
-        $self->ehrId( $response->{ehrId} );
-        $self->action( $response->{action} );
-        $self->ehrStatus( $response->{ehrStatus} );
-        return 1;
-    }
-    else {
-        $self->ehrId(0);
-        $self->action('FAIL');
-        $self->err_msg( $self->response );
-        return 0;
-    }
-}
-
-sub _get_new_ehr {
+sub get_new_ehr {
     my $self = shift;
     $self->query(
-        {   subjectId        => $self->subjectId,
-            subjectNamespace => $self->subjectNamespace,
-            committerName    => $self->committerName,
+        {   subjectId        => $self->subject_id,
+            subjectNamespace => $self->subject_namespace,
+            committerName    => $self->committer_name,
+            committerId      => $self->committer_id,
         }
     );
     $self->method('POST');
     $self->submit_rest_call;
     if ( $self->response_code eq '201' ) {
         my $response = from_json( $self->response );
-        $self->ehrId( $response->{ehrId} );
-        $self->action( $response->{action} );
+        $self->_set_ehr_id( $response->{ehrId}        );
+        $self->_set_action( $response->{action}       );
+        $self->_set_href(   $response->{meta}->{href} );
         return 1;
     }
     else {
-        $self->ehrId(0);
-        $self->action('FAIL');
-        $self->err_msg( $self->response );
+        $self->_set_ehr_id(0);
+        $self->_set_action('FAIL');
+        $self->_set_err_msg( $self->response );
         return 0;
     }
 }
 
-sub _find_by_subject_id {
+sub find_by_subject_id {
     my $self = shift;
     $self->query(
-        {   subjectId        => $self->subjectId,
-            subjectNamespace => $self->subjectNamespace,
+        {   subjectId        => $self->subject_id,
+            subjectNamespace => $self->subject_namespace,
         }
     );
     $self->submit_rest_call;
     if ( $self->response_code eq '200' ) {
         my $response = from_json( $self->response );
-        $self->ehrId( $response->{ehrId} );
-        $self->action( $response->{action} );
-        $self->ehrStatus( $response->{ehrStatus} );
+        $self->_set_ehr_id(     $response->{ehrId}        );
+        $self->_set_action(     $response->{action}       );
+        $self->_set_ehr_status( $response->{ehrStatus}    );
+        $self->_set_href(       $response->{meta}->{href} );
         return 1;
     }
     else {
-        $self->ehrId(0);
-        $self->action('FAIL');
-        $self->err_msg( $self->response );
+        $self->_set_ehr_id(0);
+        $self->_set_action('FAIL');
+        $self->_set_err_msg( $self->response );
         return 0;
     }
+}
+
+sub find_by_ehrid {
+    my ( $self, $ehr_id ) = @_;
+    $self->resource("ehr/$ehr_id");
+    $self->submit_rest_call;
+    if ( $self->response_code eq '200' ) {
+        my $response = from_json( $self->response );
+        $self->_set_ehr_id(     $response->{ehrId}        );
+        $self->_set_action(     $response->{action}       );
+        $self->_set_ehr_status( $response->{ehrStatus}    );
+        $self->_set_href(       $response->{meta}->{href} );
+        return 1;
+    }
+    else {
+        $self->_set_ehr_id(0);
+        $self->_set_action('FAIL');
+        $self->_set_err_msg( $self->response );
+        return 0;
+    }
+}
+
+
+sub update_ehr_status {
+    my ($self, $status) = @_;
+    $self->resource('ehr/' . $self->ehr_id . '/status');
+    $self->headers([['Content-Type', 'application/json']]);
+    $self->method('PUT');
+    $self->submit_rest_call( to_json( $status ) );
+    if ( $self->response_code eq '200' ) {
+        my $response = from_json( $self->response );
+        $self->_set_action(     $response->{action}       );
+        $self->_set_href(       $response->{meta}->{href} );
+        return 1;
+    }
+    elsif ($self->response_code eq '500') {
+        $self->_set_action('DUPLICATE');
+        $self->_set_err_msg( 'Server Response: '. $self->response_code
+            . "\nEHR status already in use for another EHR");
+        return 0;
+    }
+    else {
+        $self->_set_action('FAIL');
+        $self->_set_err_msg( 'Server Response: '. $self->response_code
+            . "\n" . $self->response);
+        return 0;
+    }
+}
+
+sub _check_query {
+    my $self = shift;
+    croak 'No subjectId specified in query' unless $self->subject_id;
+    croak 'No subjectNamespace specified in query'
+        unless $self->subject_namespace;
+    return 1;
 }
 
 no Moose;
@@ -111,77 +154,192 @@ __END__
 
 =head1 NAME
 
-OpenEHR::REST::EHR - Interface to EHR endpoint
+OpenEHR::REST::EHR - Interface to /ehr REST endpoint
 
 =head1 VERSION
 
-This document describes OpenEHR::REST::EHR version 0.0.1
+This document describes OpenEHR::REST::EHR version 0.0.2
 
 
 =head1 SYNOPSIS
 
     use OpenEHR::REST::EHR;
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+    my $ehr = OpenEHR::Model::EHR->new(
+        {   subjectId        => '10203040',
+            subjectNamespace => 'MyProject',
+            committerName    => 'Committer Name',
+            commmitterId     => 'comnam1',
+        }
+    );
+    $ehr->find_or_new()
+        or die "Something went awry: " . $ehr->err_msg();
+    if ( $ehr->action eq 'CREATE' ) {
+        say "New ehr created. ID: ", $ehr->ehr_id;
+    }
+    elsif ( $ehr->action eq 'RETRIEVE' ) {
+        say "Found ehr for subject. ID: ", $ehr->ehr_id;
+    }
+    else {
+        say "Query action: ", $ehr->action;
+    }
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
-
+Implements REST calls for creating, retrieving and updating Ehr records
+from an OpenEHR system
 
 =head1 INTERFACE 
 
+=head1 METHODS
+
+=head2 new ( [%subject] )
+
+Constructs a new OpenEHR::REST::EHR object. Takes an optional hash or 
+hash reference containing subject data. The hash keys can be used
+as both get/set accessors. The keys are:
+
+=over 4
+
+=item subject_id 
+
+The subject identifier used outside the OpenEHR system
+
+=item subject_namespace
+
+The namespace issuing the subjectId value
+
+=item committer_name
+
+The name of the person committing the ehr record
+
+=item committer_id
+
+The identifier of the person committing the ehr record
+
+=back
+
+=head2 find_or_new 
+
+Searches the OpenEHR system for an EHR record matching the OpenEHR::REST::EHR
+object's subject_id and subject_namespace properties or requests an new EHR 
+record using these values and optionally the committer_name and committer_id
+properties.
+
+Sets the ehrId and ehrStatus properties with values returned from the search.
+If an existing record is found, the action property is set to the value 
+'RETRIEVE'. If a new record is created, then the action property is set to 
+'CREATE'. 
+
+This is a wrapper method that first calls find_by_subject_id() and, 
+if that fails, calls get_new_ehr().
+
+=head2 find_by_subject_id
+
+Searches the database for an EHR record matching the subject_id and 
+subject_namespace properties of the OpenEHR::REST::EHR object.
+If a record is found, then the ehrId and ehrStatus properties are set 
+to the values returned from the search and the action property is set to 
+'RETRIEVE'. If no record is found, then ehrId is set to zero, action is set to 'FAIL' 
+and err_msg is set to the error message returned from the server.
+Implements a GET request to '/ehr' endpoint.
+
+=head2 get_new_ehr
+
+Requests a new ehr record from database using the subject_id and 
+subject_namespace properties of the OpenEHR::REST::EHR object and, optionally, 
+using the committer_name and committer_id properties of the object. 
+Implements a POST request to '/ehr' endpoint.
+
+=head2 find_by_ehrid($ehr_id)
+
+Searches the OpenEHR database for an EHR matching the given ehr_id value. 
+If a record is found, sets the ehr_id and ehr_status in the object, and
+set the object's action property to 'RETRIEVE'. If the search fails to 
+find a matching record the ehr_id is set to zero in the object, the action
+property is set to 'FAIL' and the err_msg property is set to the error
+message returned by the server.
+Implements a GET request to '/ehr/{ehrid}'
+
+=head2 update_ehr_status (%status)
+
+Updates the ehrStatus of an ehr. Requires a hash or hashref specify the
+ehrStatus values to update. The hash keys can be any combination of:
+
+=over 4
+
+=item subjectId
+
+The subject identifier used outside the OpenEHR system
+
+=item subjectNamespace
+
+The namespace issuing the subjectId value
+
+=item queryable
+
+Boolean value to indicate if the EHR is queryable
+
+=item modifiable
+
+Boolean value to indicate if the EHR is modifiable
+
+=back
+
+If the call succeeds the action property is set to 'UPDATE' and the
+href property will contain a link to the the updated EHR. Method 
+will fail if you try to add an ehrStatus to an EHR when these values
+are already in use for another EHR. If you encounter this error the 
+action property is set to 'DUPLICATE'. For other errors the action 
+property is set to 'FAIL' and err_msg is set to the server response.
+Implements a PUT request to '/ehr/{ehrid}/status'
+
+
 =head1 ATTRIBUTES
 
-=head2 ehrId
+=head2 ehr_id
 
-The id value for an OpenEHR record
+Read-only string used to hold the ehrid/uid/value returned by the server
 
 =head2 action
 
-The action status returned from a REST call
+Read-only string holding the action status returned by the server
 
-=head2 ehrStatus
+=head2 ehr_status
 
-The ehrStatus values returned from a find request
+Read-only hashref holding the ehrStatus returned by the server
 
 =head2 err_msg
 
-Contains the latest error message if any. Should be tested 
-after a call to find methods
+Read-only string that contains the latest error message, if any, 
+returned by a failed REST call.
 
-=head1 METHODS
+=head2 href
 
-=head2 find_or_new
+Read-only string that holds the href value returned by the server
 
-my $ehr = OpenEHR::Model::EHR->new({
-	subjectId => '10203040',
-	subjectNamespace => 'MyProject'
-	committerName => 'Committer Name'};
-$ehr->find_or_new()
+=head2 resource
 
-Searches the database for an EHR record matching specified subjectId and
-subjectNamespace. If found, sets EHR object with values found. 
-Otherwise will request a new EHR from database and set object with these values.
+Used to get or set the relative path for the REST call. Default value
+is '/ehr'.
 
-=head2 find_by_ehrid
+=head2 subject_id 
 
-Searches the OpenEHR database for the given ehrId. Stores the ehrId in the object or 0 on failure
+Holds the subject identifier used outside the OpenEHR system. Corresponds to
+the e/ehr_status/subject/external_ref/id/value field in OpenEHR
 
-=head2 _get_new_ehr
+=head2 subject_namespace
 
-Private method used by find_or_new to create a new ehr
+Holds the namespace issuing the subject id. Corresponds to
+the e/ehr_status/subject/external_ref/id/namespace field in OpenEHR
 
-=head2 _find_by_subject_id
+=head2 committer_name
 
-Private method used by find_or_new to retrieve an existing ehr
+The name of the person committing the ehr record
 
+=head2 committer_id
+
+The identifier of the person committing the ehr record
 
 =head1 DIAGNOSTICS
 
--- t/35OpenEHR-REST-EHR.t
+++ t/35OpenEHR-REST-EHR.t
@@ -8,48 +8,66 @@ BEGIN {
 }
 
 diag("Testing OpenEHR::REST::EHR $OpenEHR::REST::EHR::VERSION");
-my $config         = OpenEHR::REST->new();
+ok( my $config = OpenEHR->new(),
+    'Configuration available via OpenEHR module' );
+
 my $test_ehrid     = $config->test_ehrid;
 my $test_subjectid = $config->test_subjectid;
 
 ok( my $ehr1 = OpenEHR::REST::EHR->new(),
     'Constructor called with no parameters'
 );
+
+( note 'Testing read-only properties' );
+eval { $ehr1->ehr_id('2833483843'); };
+ok( $@, 'Cannot set ehr_id property directly' );
+eval { $ehr1->action('FAIL'); };
+ok( $@, 'Cannot set action property directly' );
+eval { $ehr1->err_msg('Something went wrong!'); };
+ok( $@, 'Cannot set err_msg property directly' );
+eval { $ehr1->ehr_status( { queryable => 1, modifiable => 0 } ); };
+ok( $@, 'Cannot set ehr_status property directly' );
+
 note('Testing find_or_new method');
 eval { $ehr1->find_or_new(); };
 ok( $@, 'find_or_new fails if no parameters are provided' );
-
-my $ehr2 = OpenEHR::REST::EHR->new( subjectId => $test_subjectid );
+my $ehr2 = OpenEHR::REST::EHR->new( subject_id => $test_subjectid );
 eval { $ehr2->find_or_new(); };
-ok( $@, "find_or_new fails if only subjectId is specified" );
-
-my $ehr3 = OpenEHR::REST::EHR->new( subjectNamespace => 'GEL' );
+ok( $@, "find_or_new fails if only subject_id is specified" );
+my $ehr3 = OpenEHR::REST::EHR->new( subject_namespace => 'GEL' );
 eval { $ehr3->find_or_new(); };
-ok( $@, "find_or_new fails if only subjectNamespace is specified" );
-
-my $ehr4 = OpenEHR::REST::EHR->new( committerName => 'Committer Name' );
+ok( $@, "find_or_new fails if only subject_namespace is specified" );
+my $ehr4 = OpenEHR::REST::EHR->new( committer_name => 'Committer Name' );
 eval { $ehr4->find_or_new(); };
-ok( $@, "find_or_new fails if only committerName specified" );
+ok( $@, "find_or_new fails if only committer_name specified" );
 
+note('Testing find_or_new for existing record');
 ok( my $ehr5 = OpenEHR::REST::EHR->new(
-        subjectId        => $test_subjectid,
-        subjectNamespace => 'GEL',
-        committerName    => 'David Ramlakhan',
+        {   subject_id        => $test_subjectid,
+            subject_namespace => 'GEL',
+        }
     ),
     'Constructor called with test_subject record details'
 );
-
 ok( $ehr5->find_or_new, 'find_or_new method called for existing record' );
-is( $ehr5->action, 'RETRIEVE', "action is RETRIEVE" );
-ok( $ehr5->ehrId, "ehrId accessor" );
-is( $ehr5->ehrId, $test_ehrid, 'EhrId retrieved matches test subject ehrid' );
 ok( !$ehr5->err_msg, "Error Message not set" );
+is( $ehr5->action, 'RETRIEVE', "action is RETRIEVE" );
+is( $ehr5->ehr_id, $test_ehrid,
+    'EhrId retrieved matches test subject ehrid' );
+is( $ehr5->ehr_status->{subjectId}, $ehr5->subject_id,
+    'Returned subject id matches submitted value');
+is( $ehr5->ehr_status->{subjectNamespace}, $ehr5->subject_namespace,
+    'Returned subject namespace matches submitted value');
+note('EHR can be found at ' . $ehr5->href);
 
+
+
+note('Testing find_or_new for probable non-existing record');
 my $subjectId = int( rand(100000000) );
 ok( my $ehr6 = OpenEHR::REST::EHR->new(
-        {   subjectId        => $subjectId,
-            subjectNamespace => 'GEL',
-            committerName    => 'Committer Name',
+        {   subject_id        => $subjectId,
+            subject_namespace => 'GEL',
+            committer_name    => 'Committer Name',
         }
     ),
     'Constructor called with random subjectId'
@@ -57,34 +75,60 @@ ok( my $ehr6 = OpenEHR::REST::EHR->new(
 ok( $ehr6->find_or_new,
     'find_or_new method called for probable non-existing record' );
 is( $ehr6->action, 'CREATE', "action is CREATE" );
-ok( $ehr6->ehrId, "ehrId accessor" );
+ok( $ehr6->ehr_id, "ehr_id accessor" );
+ok( !$ehr6->ehr_status, 'EHR status not set after call to get new');
+note('EHR can be found at ' . $ehr6->href);
 
 note('Testing find_by_ehrid method');
 my $ehr7 = OpenEHR::REST::EHR->new();
-ok( !$ehr7->ehrId,                     'EhrId not set at construction' );
-ok( $ehr7->find_by_ehrid($test_ehrid), "Find existing EHR by ehrId" );
-is( $ehr7->ehrId,  $test_ehrid, "Found record matches searched record" );
+ok( !$ehr7->ehr_id,                    'Ehr_id not set at construction' );
+ok( $ehr7->find_by_ehrid($test_ehrid), "Find existing EHR by ehr_id" );
 is( $ehr7->action, 'RETRIEVE',  "Action is RETRIEVE" );
-ok( $ehr7->ehrStatus,               "response ehrStatus accessor" );
-ok( $ehr7->ehrStatus->{queryable},  "Status queryable true" );
-ok( $ehr7->ehrStatus->{modifiable}, "Status modifiable true" );
-ok( !$ehr7->err_msg,                "Error Message not set" );
+is( $ehr7->ehr_id, $test_ehrid, "Found record matches searched record" );
+ok( $ehr7->ehr_status,               "response ehr_status accessor" );
+ok( $ehr7->ehr_status->{queryable},  "Status queryable true" );
+ok( $ehr7->ehr_status->{modifiable}, "Status modifiable true" );
+ok( !$ehr7->err_msg,                 "Error Message not set" );
+ok( $ehr7->ehr_status->{subjectId}, 'Subject id set');
+ok( $ehr7->ehr_status->{subjectNamespace}, 'Subject namespace set');
+note('EHR can be found at ' . $ehr7->href);
 
 my $ehr8 = OpenEHR::REST::EHR->new();
 ok( !$ehr8->find_by_ehrid('1232323'), "Failed to find non-existent EHR" );
-ok( !$ehr8->ehrId,                    "No ehrId set" );
-ok( !$ehr8->ehrStatus,                "ehrStatus not set" );
+ok( !$ehr8->ehr_id,                   "No ehr_id set" );
+ok( !$ehr8->ehr_status,               "ehr_status not set" );
 is( $ehr8->action, 'FAIL', "Action is FAIL" );
 ok( $ehr8->err_msg, "Error Message Set" );
 
 my $ehr9 = OpenEHR::REST::EHR->new();
-ok( !$ehr9->ehrId,                        'EhrId not set at construction' );
-ok( $ehr9->find_by_ehrid( $ehr6->ehrId ), "Find new EHR by ehrId" );
-is( $ehr9->ehrId,  $ehr6->ehrId, "Found record matches searched record" );
-is( $ehr9->action, 'RETRIEVE',   "Action is RETRIEVE" );
-ok( $ehr9->ehrStatus,               "response ehrStatus accessor" );
-ok( $ehr9->ehrStatus->{queryable},  "Status queryable true" );
-ok( $ehr9->ehrStatus->{modifiable}, "Status modifiable true" );
-ok( !$ehr9->err_msg,                "Error Message not set" );
+ok( $ehr9->find_by_ehrid( $ehr6->ehr_id ), "Find new EHR by ehr_id" );
+is( $ehr9->ehr_id, $ehr6->ehr_id, "Found record matches searched record" );
+is( $ehr9->action, 'RETRIEVE',    "Action is RETRIEVE" );
+ok( $ehr9->ehr_status,               "response ehr_status accessor" );
+ok( $ehr9->ehr_status->{queryable},  "Status queryable true" );
+ok( $ehr9->ehr_status->{modifiable}, "Status modifiable true" );
+ok( !$ehr9->err_msg,                 "Error Message not set" );
+ok( $ehr9->ehr_status->{subjectId}, 'Subject id set');
+ok( $ehr9->ehr_status->{subjectNamespace}, 'Subject namespace set');
+note('EHR can be found at ' . $ehr9->href);
+
+note('Testing update_ehr_status method');
+my $subject = {
+    subjectNamespace => 'GEL V3',
+    subjectId => '33232323',
+    queryable => 0, 
+    modifiable => 0,
+};
+ok( $ehr9->update_ehr_status($subject), 'Update status called');
+if ($ehr9->action eq 'DUPLICATE') {
+    diag( 'Duplicate error encountered: ' . $ehr9->err_msg);
+}
+elsif ($ehr9->action eq 'UPDATE') {
+    note('EHR can be found at ' . $ehr9->href);
+}
+else {
+    diag ( 'Unknown error encountered ' . $ehr9->err_msg );
+}
+
 
 done_testing;
