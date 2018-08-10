c553346c4e69fc20482603c4557cff24ae8c9de6
David Ramlakhan <dram1964@gmail.com>
2018-08-10 15:45:08 +0100

    Added some test data

---
+++ script/dump_data.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use Genomes_100K::Model;
+
+my $schema = Genomes_100K::Model->connect('CRIUGenomesTest');
+
+my $lab_results_rs = $schema->resultset('PathologyResult');
+
+while ( my $result = $lab_results_rs->next ) {
+    print
+      join( "\t", $result->order_code, $result->test_code, $result->result, $result->result_date, $result->authorisation_date, 
+        $result->units, $result->range_low, $result->range_high, $result->report, $result->wp_function),
+      "\n";
+}
