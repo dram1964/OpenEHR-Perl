9f76e96d5bfa134e07c23072326b56b4c3a13072
David Ramlakhan <dram1964@gmail.com>
2019-02-28 09:57:06 +0000

    Added some POD to find_by_uid script

--- script/find_by_uid.pl
+++ script/find_by_uid.pl
@@ -5,6 +5,9 @@ use Data::Dumper;
 =head1  find_by_UID composition_uid composition_format
 
 Use this script to dump a composition in the format specified
+find_by_uid.pl \
+0405d5d0-63be-44d8-86eb-d6e50d881dad::default::1 \
+[ FLAT | STRUCTURED | RAW ]
 
 =cut
 
