4f4485b7ccee6f490a917f0b3198f17e7c8920bb
David Ramlakhan <dram1964@gmail.com>
2019-04-10 12:06:58 +0100

    Updating set_cancer_expiry_date to handle single resultsets

--- script/cancer/set_cancer_expiry_date.pl
+++ script/cancer/set_cancer_expiry_date.pl
@@ -4,12 +4,10 @@ use Genomes_100K::Model;
 
 my $schema = Genomes_100K::Model->connect('CRIUGenomes');
 
-my $exclusion_list = &get_unsubmitted_ids;
-my $inclusion_list = &get_submitted_ids;
+my $exclusions = &get_unsubmitted_ids;
+my $inclusions = &get_submitted_ids;
 
-print "Exclusion List: ", join( ":", @{ $exclusion_list } ), "\n";
-
-&update_expiry_date($exclusion_list, $inclusion_list);
+&update_expiry_date();
 
 =head2 &update_expiry_date($exclustions)
 
@@ -22,45 +20,62 @@ sub update_expiry_date {
 
     my $update_rs = $schema->resultset('InformationOrder')->search(
         {
-            expiry_date => undef,
+            expiry_date  => undef,
             service_type => 'cancer',
-            order_state => 'scheduled',
+            order_state  => 'scheduled',
         },
         {
-            columns => [ qw/ subject_id / ],
+            columns  => [qw/ subject_id /],
             distinct => 1,
         }
     );
     if ( $update_rs->count > 0 ) {
-        while ( my $update = $update_rs->next ) {
-            my $nhs_number = $update->subject_id;
-            if ( grep { /$nhs_number/ }  @{ $exclusions } ) { 
-                print "Subject ID: " . $nhs_number . ", found in exclusion list. Skipping\n";
-            }
-            elsif ( grep { /$nhs_number/ } @{ $inclusions } ) {
-                print "Subject ID: " . $nhs_number . ", found in inclusion list. Updating\n";
-                my $order_rs = $schema->resultset('InformationOrder')->search(
-                    {
-                        service_type => 'cancer',
-                        subject_id =>  $nhs_number,
-                    }
-                );
-                if ( $order_rs->count > 1 ) {
-                    while ( my $order = $order_rs->next ) {
-                        my $end_date = $order->data_end_date;
-                        $order->update( { expiry_date => $end_date } );
-                    }
-                }
-                else {
-                    my $order = $order_rs->first;
-                    my $end_date = $order->data_end_date;
-                    $order->update( { expiry_date => $end_date } );
-                }
-            }
-            else {
-                print "Subject ID: " . $nhs_number . " has no submitted compositions. Skipping\n";
+        if ( $update_rs->count > 1 ) {
+            while ( my $update = $update_rs->next ) {
+                &set_expiry_date($update);
             }
         }
+        else {
+            my $update     = $update_rs->first;
+            &set_expiry_date($update);
+        }
+    }
+}
+
+sub set_expiry_date {
+    my $update     = shift;
+    my $nhs_number = $update->subject_id;
+    if ( grep { /$nhs_number/ } @{$exclusions} ) {
+        print "Subject ID: "
+          . $nhs_number
+          . ", found in exclusion list. Skipping\n";
+    }
+    elsif ( grep { /$nhs_number/ } @{$inclusions} ) {
+        print "Subject ID: "
+          . $nhs_number
+          . ", found in inclusion list. Updating\n";
+        my $order_rs = $schema->resultset('InformationOrder')->search(
+            {
+                service_type => 'cancer',
+                subject_id   => $nhs_number,
+            }
+        );
+        if ( $order_rs->count > 1 ) {
+            while ( my $order = $order_rs->next ) {
+                my $end_date = $order->data_end_date;
+                $order->update( { expiry_date => $end_date } );
+            }
+        }
+        else {
+            my $order    = $order_rs->first;
+            my $end_date = $order->data_end_date;
+            $order->update( { expiry_date => $end_date } );
+        }
+    }
+    else {
+        print "Subject ID: "
+          . $nhs_number
+          . " has no submitted compositions. Skipping\n";
     }
 }
 
@@ -75,10 +90,10 @@ sub get_submitted_ids {
     my @ids;
     my $submitted_rs = $schema->resultset('InfoflexCancer')->search(
         {
-            composition_id =>  { -not => undef } ,
+            composition_id => { -not => undef },
         },
         {
-            columns => [ qw/ nhs_number / ],
+            columns  => [qw/ nhs_number /],
             distinct => 1,
         }
     );
@@ -99,10 +114,10 @@ sub get_unsubmitted_ids {
     my @ids;
     my $unsubmitted_rs = $schema->resultset('InfoflexCancer')->search(
         {
-            composition_id =>  undef ,
+            composition_id => undef,
         },
         {
-            columns => [ qw/ nhs_number / ],
+            columns  => [qw/ nhs_number /],
             distinct => 1,
         }
     );
